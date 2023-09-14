8e220c53a2565e48d10b585c83642bf249df33d0
Shaxzoda <141127730+abduxakimova@users.noreply.github.com>
2023-09-14 15:06:13 +0500

    js ozgardi

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-fetch ('https://api.openweathermap.org/data/2.5/weather?q=China&units=metric&appid=${API_KEY}')
-.then((res)=> res.json ())
+fetch (`https://api.openweathermap.org/data/2.5/weather?q=China&units=metric&appid=33dedde6287575d237be2e1c44271762`)
+.then((res)=> res.json())
 .then ( (data) => {
     console.log(data);
 })
