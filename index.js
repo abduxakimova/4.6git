d0fef4957a619f0292d403a334a3fe5152da04e1
Shaxzoda <141127730+abduxakimova@users.noreply.github.com>
2023-09-14 14:59:21 +0500

    fetch+

--- index.js
+++ index.js
@@ -3,7 +3,3 @@ fetch ('https://api.openweathermap.org/data/2.5/weather?q=China&units=metric&app
 .then ( (data) => {
     console.log(data);
 })
-
-// Api manzili
-// API_KEY = "33dedde6287575d237be2e1c44271762";
-//  file_get_contents = ("https://api.openweathermap.org/data/2.5/weather?q=Namangan&units=metric&appid=${API_KEY}");
